31117aca739077bbc080cb09980acd5582f46eaf
satyamshete <shetesatyam99@gmail.com>
2022-08-11 17:18:49 +0530

    [Satyam] Add: UC-2 Leap Year Programme

---
+++ FunctionalProgrammingProblems/LeapYearCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class LeapYearCheck
+    {
+        internal void CheckLeapYear()
+        {
+            Console.WriteLine("Leap Year Programme");
+            Console.WriteLine("Enter a year");
+            int year = int.Parse(Console.ReadLine());
+            while (year < 1000 || year > 9999)
+            {
+                Console.WriteLine("Please enter 4 digit value");
+                year = int.Parse(Console.ReadLine());
+            }
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+
+
+                Console.WriteLine("Given Year is Leap Year ");
+
+            }
+            else
+            {
+                if (year % 100 != 0 && year % 4 == 0)
+                {
+                    Console.WriteLine("Given Year is Leap Year ");
+                }
+                else
+                {
+                    Console.WriteLine("Given Year isnot a Leap Year ");
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
+
--- FunctionalProgrammingProblems/Program.cs
+++ FunctionalProgrammingProblems/Program.cs
@@ -6,12 +6,18 @@
         {
             Console.WriteLine("Choose your selection from below");
             Console.WriteLine("1. Flip Coin Percentage");
+            Console.WriteLine("2. Leap Year");
             int n = Convert.ToInt32(Console.ReadLine());
             switch (n)
             {
                 case 1:
                     PercentageCoinFlip percentageCoinFlip = new PercentageCoinFlip();
                     percentageCoinFlip.FlipCoinPercentage();
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    LeapYearCheck leapYearCheck = new LeapYearCheck();
+                    leapYearCheck.CheckLeapYear();
                     Console.WriteLine();
                     break;
                 default:
